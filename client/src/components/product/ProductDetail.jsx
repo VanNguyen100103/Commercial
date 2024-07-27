ed3a1777a7d055e719487f0b429585b8d31fb197
VanNguyen100103 <nganhvan1609@gmail.com>
2024-07-27 22:32:00 +0700

    refactor: github repo

--- client/src/components/product/ProductDetail.jsx
+++ client/src/components/product/ProductDetail.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import ImageZoom from 'react-image-zoom';
+import InnerImageZoom from 'react-inner-image-zoom';
+import Slider from 'react-slick';
+import 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { getProductDetails } from '../../redux/actions/productActions';
 
 const ProductDetail = () => {
@@ -23,19 +27,40 @@ const ProductDetail = () => {
   if (error) return <p>Error: {error}</p>;
   if (!product) return <p>Product not found</p>;
 
-  const imageUrl = product.images && product.images.length > 0 ? product.images[0].url : '../../assets/images/default_avatar.png';
+  const renderImages = () => {
+    if (product.images && product.images.length > 0) {
+      // Use a Set to ensure unique images
+      const uniqueImages = [...new Set(product.images.map(image => image.url))];
+      return uniqueImages.map((url, index) => (
+        <div key={index} style={{ margin: '10px' }}>
+          <InnerImageZoom src={url} zoomSrc={url} zoomType="hover" />
+        </div>
+      ));
+    } else {
+      return (
+        <div style={{ margin: '10px' }}>
+          <InnerImageZoom src='../../assets/images/default_avatar.png' zoomSrc='../../assets/images/default_avatar.png' zoomType="hover" />
+        </div>
+      );
+    }
+  };
 
-  const zoomProps = {
-    width: 400,
-    height: 300,
-    zoomWidth: 500,
-    img: imageUrl // Ensure there's a default image
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
   };
 
   return (
     <div className='product-detail'>
       <h1>{product.name}</h1>
-      <ImageZoom {...zoomProps} />
+      <div style={{width: "40rem", height: "20rem", margin: "auto"}} className='product-images'>
+        <Slider {...settings}>
+          {renderImages()}
+        </Slider>
+      </div>
       <p>{product.description}</p>
       <p>Price: ${product.price}</p>
     </div>
